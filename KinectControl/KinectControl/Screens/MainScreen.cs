56c7eb6609504bf136542d35e3b34348b06e021d
Khaled <khaled.osmaan@gmail.com>
2013-04-15 20:42:48 +0200

    t test
    t
    quit
    enter
    
    Thesis introduction

--- KinectControl/KinectControl/Screens/MainScreen.cs
+++ KinectControl/KinectControl/Screens/MainScreen.cs
@@ -6,6 +6,8 @@ using Microsoft.Xna.Framework.Graphics;
 using KinectControl.UI;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework;
+using KinectControl.Common;
+using Microsoft.Kinect;
 
 namespace KinectControl.Screens
 {
@@ -13,6 +15,11 @@ namespace KinectControl.Screens
     {
         private SpriteBatch spriteBatch;
         private SpriteFont font;
+        private Kinect kinect;
+        private HandCursor Hand;
+        private SkeletonAnalyzer analyzer;
+        private string text;
+        double angle = 0;
         private GraphicsDevice graphics;
         private int screenWidth,screenHeight;
         private Button button;
@@ -31,6 +38,9 @@ namespace KinectControl.Screens
             button = new Button();
             hand = new HandCursor();
             hand.Initialize(ScreenManager.Kinect);
+            analyzer = new SkeletonAnalyzer();
+            if (!(ScreenManager.Kinect.trackedSkeleton == null))
+                analyzer.SetBodySegments(ScreenManager.Kinect.trackedSkeleton.Joints[JointType.HipLeft], ScreenManager.Kinect.trackedSkeleton.Joints[JointType.HipCenter], ScreenManager.Kinect.trackedSkeleton.Joints[JointType.HipRight]);
             button.Initialize("Buttons/OK", this.ScreenManager.Kinect, new Vector2(820, 100));
             button.Clicked += new Button.ClickedEventHandler(button_Clicked);
             base.Initialize();
@@ -47,6 +57,7 @@ namespace KinectControl.Screens
             spriteBatch = ScreenManager.SpriteBatch;
             screenHeight = graphics.Viewport.Height;
             screenWidth = graphics.Viewport.Width;
+            kinect = ScreenManager.Kinect;
             gradientTexture = content.Load<Texture2D>("Textures/gradientTexture");
             font = content.Load<SpriteFont>("SpriteFont1");
             hand.LoadContent(content);
@@ -57,6 +68,9 @@ namespace KinectControl.Screens
         {
             hand.Update(gameTime);
             button.Update(gameTime);
+            if (!(ScreenManager.Kinect.trackedSkeleton == null))
+                angle = analyzer.GetBodySegmentAngle(ScreenManager.Kinect.trackedSkeleton.Joints);
+            text = "" + angle;
             FreezeScreen();
             base.Update(gameTime);
         }
@@ -67,6 +81,7 @@ namespace KinectControl.Screens
         //    spriteBatch.Begin(SpriteSortMode.BackToFront,BlendState.AlphaBlend,null,null,null,null,cam.Transform);
             spriteBatch.Begin();
             spriteBatch.Draw(gradientTexture, new Rectangle(0, 0, 1280, 720), Color.White);
+            spriteBatch.DrawString(font, text, Vector2.Zero, Color.Orange);
             button.Draw(spriteBatch);
             hand.Draw(spriteBatch);
             spriteBatch.End();
